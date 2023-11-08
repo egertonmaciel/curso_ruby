8d9be94d56bae2d54f8f2347265d883138fcc3c2
egerton.maciel@gmail.com <egerton.maciel@gmail.com>
2023-11-08 16:18:30 -0300

    criado arquivo app

---
+++ app.rb
@@ -0,0 +1,18 @@
+a = File.open("lista.txt")
+linhas = []
+
+a.each do |linha|
+	linhas.append(linha)
+end
+
+linhas.each do |linha|
+	puts linha
+end
+
+linhas.sort!
+
+puts
+
+linhas.each do |linha|
+	puts linha
+end
